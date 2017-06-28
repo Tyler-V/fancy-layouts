b9d34ea888e9f6076d55f51a8b023926a338624c
Tyler Vorpahl <tvorpahl@transfinder.com>
2017-06-28 17:04:39 -0400

    Base

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# fancy-layouts
+# modular-layout
+
+# Visual studio Extensions
+Angular Language Service
+Easy LESS
+JS-CSS-HTML Formatter
+Stylesheet Formatter
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { ContentComponent } from './components/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavigationComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
